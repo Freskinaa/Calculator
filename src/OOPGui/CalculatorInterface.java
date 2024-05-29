d3f2dc2188da633c25d50fb30f3f30b35d7f493a
Freskina Selimi <sfreskina@gmail.com>
2024-05-29 12:54:05 +0200

    Calculator Project OOPGUI

---
+++ src/OOPGui/CalculatorInterface.java
@@ -0,0 +1,8 @@
+package OOPGui;
+public interface CalculatorInterface {
+    void appendToDisplay(String str);
+    void repaintFont();
+    void updateDisplay(double val);
+    void updateDisplay(String val);
+    void showError(String message);
+}
---
+++ src/OOPGui/Main.java
@@ -0,0 +1,7 @@
+package OOPGui;
+
+public class Main {
+    public static void main(String[] args) {
+        new Calculator();
+    }
+}
